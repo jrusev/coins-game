1a7d60a31de53d966c88a8d7846b8176efd55785
jrusev <jrusev@gmail.com>
2019-08-17 22:18:55 +0300

    Limit movement inside the area

--- index.js
+++ index.js
@@ -26,13 +26,22 @@ function main() {
     draw();
 }
 
+function isAllowed(x, y) {
+    return x >= 0 && x <= canvas.width - width && y >= 0 && y <= canvas.height - height
+}
+
 function draw() {
-    if (keys && keys[37]) { x += -delta; }
-    if (keys && keys[39]) { x += +delta; }
-    if (keys && keys[38]) { y += -delta; }
-    if (keys && keys[40]) { y += +delta; }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, width, height);
+    dx = dy = 0
+    if (keys && keys[37]) { dx = -delta; }
+    if (keys && keys[39]) { dx = +delta; }
+    if (keys && keys[38]) { dy = -delta; }
+    if (keys && keys[40]) { dy = +delta; }
+    if (isAllowed(x + dx, y + dy)) {
+        x += dx;
+        y += dy;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, width, height);
+    }
     requestAnimationFrame(draw);
 }
